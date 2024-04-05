fda6205ed980f373d0f0e7f89c86dbce77c3d64f
Danilo N. B. <danilobenatti@gmail.com>
2024-04-05 18:39:26 -0300

    Add properties for connection with database

--- src/main/java/br/com/ecosensor/util/ConnectionDB.java
+++ src/main/java/br/com/ecosensor/util/ConnectionDB.java
@@ -1,46 +1,64 @@
 package br.com.ecosensor.util;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import static org.apache.commons.lang3.StringUtils.join;
+import static org.apache.commons.lang3.StringUtils.joinWith;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import static org.apache.commons.lang3.StringUtils.join;
-import static org.apache.commons.lang3.StringUtils.joinWith;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ConnectionDB {
 	
-	private static final String USER = "root";
-	private static final String PASSWORD = "123456";
+	private static final String USESSL = "useSSL=true";
+	private static final String CERTIFICATE = "verifyServerCertificate=false";
 	private static final String ENCODING = "useUnicode=true;characterEncoding=UTF-8";
 	private static final String TIMEZONE = "useTimezone=true;serverTimezone=America/Sao_Paulo";
-	private static final String URL = "jdbc:mysql://localhost:3306/java_course";
 	
 	private static BasicDataSource dataSource;
+	
+	private static Properties getProperties() {
+		Properties prop = new Properties();
+		File file = new File(
+				"./src/main/java/br/com/ecosensor/util/connection.properties");
+		try (FileInputStream input = new FileInputStream(file)) {
+			prop.load(input);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return prop;
+	}
 	
 	static Logger log = LogManager.getLogger(ConnectionDB.class);
 	
 	public static BasicDataSource getDataSource() {
+		Properties prop = getProperties();
 		if (dataSource == null || dataSource.isClosed()) {
 			dataSource = new BasicDataSource();
+			dataSource.setUrl(prop.getProperty("db.url"));
+			dataSource.setDefaultSchema(prop.getProperty("db.schema"));
+			dataSource.setUsername(prop.getProperty("db.username"));
+			dataSource.setPassword(prop.getProperty("db.password"));
 			dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-			dataSource.setUrl(URL);
-			dataSource.setUsername(USER);
-			dataSource.setPassword(PASSWORD);
+			dataSource.setConnectionProperties(
+					joinWith(";", USESSL, CERTIFICATE, ENCODING, TIMEZONE));
+			dataSource.setDefaultAutoCommit(true);
 			dataSource.setInitialSize(3);
 			dataSource.setMinIdle(3);
 			dataSource.setMaxIdle(8);
 			dataSource.setMaxTotal(8);
-			dataSource.setConnectionProperties(joinWith(";", ENCODING, TIMEZONE));
-			dataSource.setDefaultSchema("java_course");
-			dataSource.setDefaultAutoCommit(false);
 		}
-		log.info(() -> join("Start connection: ", dataSource.getDefaultCatalog()));
+		log.info(() -> join("Start conn: ", dataSource.getDefaultSchema()));
 		return dataSource;
 	}
 	
